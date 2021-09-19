ecdd923f27f26cfb6d763cacae45bec05cedb987
eikcaj <huangyq@shanghaitech.edu.cn>
2021-09-19 19:16:33 -0400

    Submit Assignment 1

--- src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
+++ src/main/java/edu/neu/coe/info6205/randomwalk/RandomWalk.java
@@ -4,6 +4,7 @@
 
 package edu.neu.coe.info6205.randomwalk;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomWalk {
@@ -20,7 +21,8 @@ public class RandomWalk {
      * @param dy the distance he moves in the y direction
      */
     private void move(int dx, int dy) {
-        // TO BE IMPLEMENTED
+        x += dx;
+        y += dy;
     }
 
     /**
@@ -29,7 +31,9 @@ public class RandomWalk {
      * @param m the number of steps the drunkard takes
      */
     private void randomWalk(int m) {
-        // TO BE IMPLEMENTED
+        for (int i = 0; i < m; i++) {
+            randomMove();
+        }
     }
 
     /**
@@ -48,7 +52,7 @@ public class RandomWalk {
      * @return the (Euclidean) distance from the origin to the current position.
      */
     public double distance() {
-        // TO BE IMPLEMENTED
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
 
     /**
@@ -68,14 +72,39 @@ public class RandomWalk {
         return totalDistance / n;
     }
 
+    public static void variousStep(int m0, int m1, int n, ArrayList<double[]> res) {
+        for (int m = m0; m < m1 + 1; m++) {
+            double meanDistance = randomWalkMulti(m, n);
+            System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments (c = " + meanDistance / Math.sqrt(m) + ")");
+            res.add(new double[]{(double) m, meanDistance, meanDistance / Math.sqrt(m)});  // {steps, meanDistance, c}
+        }
+    }
+
     public static void main(String[] args) {
-        if (args.length == 0)
-            throw new RuntimeException("Syntax: RandomWalk steps [experiments]");
-        int m = Integer.parseInt(args[0]);
-        int n = 30;
-        if (args.length > 1) n = Integer.parseInt(args[1]);
-        double meanDistance = randomWalkMulti(m, n);
-        System.out.println(m + " steps: " + meanDistance + " over " + n + " experiments");
+        // Step 1: Do an experiment for observing.
+        ArrayList<double[]> res1 = new ArrayList<>();
+        variousStep(1, 50, 1000, res1);
+        res1.forEach(r -> System.out.println(r[0] + "\t" + r[1]));
+
+        // Step 3: Do an experiment to obtain an average value of c.
+        ArrayList<double[]> res2 = new ArrayList<>();
+        variousStep(2, 50, 1000, res2);
+        res2.forEach(r -> System.out.println(r[0] + "\t" + r[1] + "\t" + r[2]));
+        double c_avg = res2.stream().mapToDouble(c -> c[2]).average().orElse(0.0);
+        System.out.println(">>>>>>>>>> c_avg = " + c_avg);
+
+        // Step 4: Do an experiment to prove the assumption is acceptable.
+        double c_avg_4 = 0.8883323419656484;
+        ArrayList<Double> sds = new ArrayList<>();
+        ArrayList<double[]> res3 = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            res3.clear();
+            variousStep(2, 50, 1000, res3);
+            double sd = Math.sqrt(res3.stream().mapToDouble(c -> Math.pow(c[2] - c_avg_4, 2)).sum() / (res3.size() - 1));
+            System.out.println(">>> SD: " + sd);
+            sds.add(sd);
+        }
+        System.out.println(">>> SD_avg: " + sds.stream().mapToDouble(sd -> sd).average().orElse(0.0));
     }
 
 }
