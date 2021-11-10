58cfeb31886fc80ffe0c84d08be9f97fe1a48efe
eikcaj <huangyq@shanghaitech.edu.cn>
2021-11-10 12:05:44 -0500

    Complete Assignment 5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -18,12 +18,27 @@ public class Main {
 
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+
+        double step = 0.025;
+        int[] arraySizes = {1000000, 2000000, 4000000, 8000000, 16000000};
+        int[] threadNums = {1, 2, 4, 8};
+
+        for (int arraySize : arraySizes) {
+            for (int threadNum : threadNums) {
+                runExperiment(arraySize, threadNum, step);
+            }
+        }
+    }
+
+    private static void runExperiment(int arraySize, int threadNum, double step) {
+        ParSort.myPool = new ForkJoinPool(threadNum);
+        System.out.println("Degree of parallelism: " + ParSort.myPool.getParallelism());
+//        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[arraySize];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+        for (double j = 0.05; j <= 1.0; j += step) {
+            ParSort.cutoff = (int) (arraySize * j);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
@@ -40,13 +55,13 @@ public class Main {
 
         }
         try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
+            FileOutputStream fis = new FileOutputStream(String.format("./src/results/size_%d_thread_%d.csv", arraySize, threadNum));
             OutputStreamWriter isr = new OutputStreamWriter(fis);
             BufferedWriter bw = new BufferedWriter(isr);
-            int j = 0;
+            double j = 0.05;
             for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
-                j++;
+                String content = j + "," + (double) i / 10 + "\n";
+                j += step;
                 bw.write(content);
                 bw.flush();
             }
